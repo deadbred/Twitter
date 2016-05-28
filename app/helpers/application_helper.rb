a60ea3010eb7ce7087864d518430dee53db941cb
deadbred <deadbred3@gmail.com>
2016-05-28 18:43:27 -0400

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	def full_title(page_title = '')
-		base_title = "Twitter"
+		base_title = "KICKS Twitter"
 		if page_title.empty?
 			base_title
 		else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Twitter"
+    assert_select "title", "KICKS Twitter"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Twitter"
+    assert_select "title", "Help | KICKS Twitter"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Twitter"
+  	assert_select "title", "About | KICKS Twitter"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | KICKS Twitter"
+  end
 end
